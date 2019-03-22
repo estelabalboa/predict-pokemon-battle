bff281ce57a55334834e3671e407cb78ae4a65e5
Estela Balboa <estela.balboa@gmail.com>
2019-03-22 15:12:24 +0100

    feat: lectura con pandas de los 3 ficheros, describe de pokemon_data

---
+++ pokemon_battle.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+from time import time
+from IPython.display import display
+import matplotlib.pyplot as plt
+
+
+class PokemonBattle:
+
+    def __init__(self):
+        pokemon_data = pd.read_csv("Dataset_Pokemon_AI6/pokemons_data.csv")
+        combats = pd.read_csv("Dataset_Pokemon_AI6/combats.csv")
+        test_data = pd.read_csv("Dataset_Pokemon_AI6/tests.csv")
+
+        # Mostrar número de filas y columnas del dataframe
+        print("Filas: " + str(pokemon_data.shape[0]) + " Columnas: " + str(pokemon_data.shape[1]))
+
+        # Mostrar las primeras 10 filas
+        display(pokemon_data.head(10))
+
+        # Descripción analítica básica del dataframe
+        print(np.round(pokemon_data.describe()))
+
+
+if __name__ == '__main__':
+    battle_result = PokemonBattle()
+
